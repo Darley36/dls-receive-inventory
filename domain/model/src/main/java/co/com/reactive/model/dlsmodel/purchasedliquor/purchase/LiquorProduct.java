33230f2eb0ccf7a47e075aa62cf687e4783eaabd
Darley36 <darleybest@gmail.com>
2022-05-17 09:57:12 -0500

    all .gradle files updated

--- domain/model/src/main/java/co/com/reactive/model/dlsmodel/purchasedliquor/purchase/LiquorProduct.java
+++ domain/model/src/main/java/co/com/reactive/model/dlsmodel/purchasedliquor/purchase/LiquorProduct.java
@@ -6,4 +6,16 @@ import lombok.Data;
 @Data
 @Builder(toBuilder = true)
 public class LiquorProduct {
+    private String idProduct;
+    private String amount;
+    private double value;
+    private double priceSell;
+    private double discount;
+    private double discountPercent;
+    private String type;
+    private String size;
+    private String color;
+    private String degressAlcohol;
+    private boolean isSweet;
+    private String comesFrom;
 }
